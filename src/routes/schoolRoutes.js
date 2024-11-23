f10f77650fb81b4498551fdb97b1c328feb37efb
Jasleen200000001 <jasskaurrupal@gmail.com>
2024-11-24 00:54:57 +0530

    om

--- src/routes/schoolRoutes.js
+++ src/routes/schoolRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { addSchool, listSchools } = require('../controllers/schoolController.js');
 
 // Routes
-router.post('/addSchool', addSchool);
-router.get('/listSchools', listSchools);
+router.post('/api/addSchool', addSchool);
+router.get('/api/listSchools', listSchools);
 
 module.exports = router;
